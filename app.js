6a789396d96021c11933fded4552f75eec15d5c6
MayerXx <meet_fate_x@icloud.com>
2025-07-28 07:59:51 +0800

    feat(db): connect to MongoDB using dotenv and mongoose module
    Project now connects to MongoDB via mongoose. Environment variables are loaded through dotenv (.env).

--- app.js
+++ app.js
@@ -5,12 +5,35 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import { fileURLToPath } from 'url';
 
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+
+// use .env variables
+dotenv.config();
+
+// judge if mongodb uri is defined
+if (!process.env.MONGODB_URI) {
+  console.error("MONGODB_URI not defined!");
+  process.exit(1);
+}
+
+// connect mongodb and add success and failed callback
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("connect mongodb successfully!")
+}).catch((err) => {
+  console.log("failed to connect mongodb!, error: ", err);
+  process.exit(1);
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
