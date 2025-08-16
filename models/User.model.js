9a68b42324919923bad367fcb6e81581f737bf57
lcys2025 <lcys2025@outlook.com>
2025-08-16 23:19:49 +0800

    (1) Removed username, (2) Added Bookig.model.js

--- models/User.model.js
+++ models/User.model.js
@@ -3,14 +3,10 @@ import validator from "validator";
 
 const userSchema = new mongoose.Schema(
 	{
-		name: { type: String, trim: true, maxlength: 30 },
-		username: {
+		name: {
 			type: String,
-			required: true,
-			unique: true,
-			minlength: 3,
-			maxlength: 30,
 			trim: true,
+			maxlength: 30
 		},
 		password: {
 			type: String,
@@ -28,6 +24,18 @@ const userSchema = new mongoose.Schema(
 				validator: (v) => validator.isEmail(v),
 				message: "Please enter a valid email",
 			},
+		},
+		plan: {
+			type: String,
+			required: true,
+			lowercase: true,
+			enum: ['basic', 'premium', 'vip'],
+		},
+		role: {
+			type: String,
+			required: true,
+			lowercase: true,
+			enum: ['gymer', 'trainer'],
 		},
 	},
 	{
